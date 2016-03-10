b0b7e15f14356a14ccdf70dfb47758b04e35cf94
David Raab <davidraab83@gmail.com>
2016-03-10 13:56:42 +0100

    Added Google Analytics

--- generate.bat
+++ generate.bat
@@ -1,5 +1,5 @@
 @echo off
 
 call .paket\paket restore
-call tools\fsformatting.exe literate --processDirectory --lineNumbers true --inputDirectory  "code" --outputDirectory "_posts"
+call tools\fsformatting.exe literate --processDirectory --lineNumbers true --inputDirectory  "code" --outputDirectory "_posts" --waitforkey
 
