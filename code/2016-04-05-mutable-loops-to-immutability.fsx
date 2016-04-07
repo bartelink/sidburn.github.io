e8f57a2d4271cc4e5bbabf0b7135e77002dae4e5
David Raab <davidraab83@gmail.com>
2016-04-07 03:56:42 +0200

    Mutable Loops to Immutability
    * Gosh we do i always write a square function wrong?
    * A rewrote a lot of sentences. I hope they are now clearer.
    * Added named arguments to the function signature
    * Two more examples for the `forEach` function.

--- code/2016-04-05-mutable-loops-to-immutability.fsx
+++ code/2016-04-05-mutable-loops-to-immutability.fsx
@@ -38,22 +38,26 @@ numbers.Add(1)
 numbers.Add(2)
 numbers.Add(3)
 
-let squaredNumbers = ResizeArray.map (fun x -> x * 2) numbers
+let squaredNumbers = ResizeArray.map (fun x -> x * x) numbers
 
 (**
-We now have two `ResizeArray`. `numbers` containing the numbers `1`, `2` and `3`. And `squaredNumbers`
-containing `2`, `4`, `6`. So while this behaves like *immutability*, it still isn't exactly
+We now have two `ResizeArray`. `numbers` still contains `1`, `2` and `3` while `squaredNumbers`
+contains `1`, `4`, `9`. So while this behaves like *immutability*, it still isn't exactly
 *immutable*. We also see that in the `map` implementation. In `map` we first create
-an empty *mutable* `newArray`. While we loop over `oldArray` we add the new elements to `newArray`.
-But adding new stuff to `newArray` is only possible because `newArray` is *mutable*!
+an empty *mutable* `newArray`. While we loop over `oldArray` we *mutate* `newArray` by adding
+the result of the transformation `f x` to `newArray`. But the only reason why we could do that 
+is because `newArray` is *mutable*!
 
 But what do we do if we have a true *immutable list* like the built-in F# list? We cannot start with
 an `empty list` just loop over `oldArray` and directly add an element to the empty list. We only can
-create a new list by pre-pending an element.
+prepend elements, but this would create a whole new list and not *mutate* the empty list.
 
-But where do we save it? Sure we can assign it to a new variable. But it doesn't
-make much sense to just create a new list inside of a loop. Actually F# supports *mutable
-variables* so as a quick fix we could define a *mutable variable* outside the loop.
+Sure we can assign the new list to a new variable inside the loop. But this doesn't make much sense
+as with the next iteration we will lose our newly created list. The thing is, looping always need
+some kind of *mutable* variable outside of the loop that we can *mutate*.
+
+Actually F# supports *mutable variables* so as a quick fix we could define a *mutable variable*
+outside the loop.
 *)
 
 let mapWithImmutableList f (list:_ list) =
@@ -63,7 +67,6 @@ let mapWithImmutableList f (list:_ list) =
     acc
 
 (**
-
 It doesn't mean we created a *mutable list* here. We still only have *immutable lists*. 
 But `acc` itself is *mutable* and can be changed to point to a new *immutable list*.
 
@@ -72,19 +75,18 @@ from the previous *loop iteration*. So `acc` only keeps track of the *last or ne
 `acc`.
 
 This implementation still has a problem as we will also *reverse* the list. The problem
-is that we only can prepend elements. So the second element get prepended to the new list.
-The last element of the old list gets transformed and prepended as the new first element.
+is that we only can *prepend* elements to a list and not *append* them.
 
-On top, we still relaying on *mutable* variables. Let's first forget about the *reverse*
-problem. Here we see in general a problem. Looping needs *mutable* variables! Sure, we
+But on top, we still relaying on *mutable* variables. Let's first forget about the *reverse*
+problem. Here we see a general problem. Looping needs *mutable* variables! Sure, we
 also can do just some *side-effects* and don't *mutate* anything, but whenever you really
 want to compute something you are forced to use *mutation* if you choose a loop.
 
-Looping construct in general just don't work nicely together. That's also the reason why
-looping is discouraged in functional programming. But it opens up the question how we
-solve the problems that *looping* solves. How can we for example *loop* through an
-*immutable list* without *looping*? And how can we achieve our goal without a
-*mutable variable*?
+Looping constructs in general just don't work nicely together with *immuability*. That's also
+the reason why looping is discouraged in functional programming. But it opens up the
+question how we solve the problems that *looping* solves. How can we for example go through an
+*immutable list* without *looping*? And how can we compute something with every element
+without accessing a *mutable variable*?
 
 The answer is: Through recursion!
 
@@ -121,7 +123,7 @@ printfn "%d" i3
 (**
 Sure, writing stuff like that is simple stupid. We don't want to increment `i` ten times, always
 create a new variable, and a new print statement. But it is still interesting that the idea
-is the same with our `map` implementation. 
+is the same with our first `map` implementation we did for `ResizeArray`.
 
 We always create a new *immutable state*, by using the previous state. And that is where functions
 come into play. Actually we are not limited to just assign the result of a calculation just to a
@@ -177,7 +179,7 @@ the starting and the stop value and just provide a function for the *loop-body* 
 what should be done at every step. The current `i` is just passed as an argument to our function
 `f`. This way we can do something with the value.
 
-Let's improve it a little bit. It feels a little bit awkward that we have
+Let's improve that solution a little bit. It feels a little bit awkward that we have
 to provide `stop` and `f` again as arguments. We can fix this problem by just creating a special
 `loop` function inside `forLoop` instead.
 *)
@@ -202,7 +204,8 @@ This has several advantages:
     them to change.
 1.  This reduces the possibility of bugs as we cannot change variables that we didn't expect to change.
     For example, previously we could write: `forLoop (i+1) (stop+1) f`
-1.  In other cases it also means we can *pre-* or *post-process* data
+1.  In other cases it also means we can *pre-* or *post-process* data before or after the loop
+    starts/finish.
 1.  Sometimes we just want to provide a fixed starting value. For example in a `sum` function
     we provide `0` as the starting value. We don't want that the user must enter `0` as a
     starting value for the recursion call!
@@ -217,21 +220,23 @@ create the sum of all numbers from 0 to 9 we could write something like this in 
         sum = sum + i;
     }
 
-Fixing this problem should be easy. We just add `sum` to the `loop` function as this is a value
-that we expect to change with every *iteration*. On top our function `f` must change. In the body
-of a loop we have access to `i` and access to `sum` outside of the loop. We get access to those
-data by passing both variables to the `f` function. But we also expect that the `f` function
-returns a new version of `sum` that is then used for the next *recursive call*.
+But we now know how to fix that problem. We just add `sum` to the `loop` function
+as this is a value that we expect to change with every *iteration*. On top our function `f` must
+change. In the body of a loop we have access to `i` and access to `sum` outside of the loop.
+With recursion we only get access to those data that we pass to the `f` function. So we also must
+pass `sum` as an argument to our `f` function. As we cannot *mutate* `sum` inside `f` we expect that
+`f` returns the new `sum` that we then use for the next *recursive call*.
 *)
 
-let rec forLoop i stop f =
+let forLoop i stop f =
     let rec loop i sum =
         if i < stop then 
             // create the next sum
             let nextSum = f i sum
+            // next recursive call with updated "i" and "sum"
             loop (i+1) nextSum
         else
-            // Return "sum" if we reached the end
+            // Return "sum" as soon we reach stop
             sum
     loop i 0
 
@@ -241,13 +246,15 @@ let sum = forLoop 0 10 (fun i sum -> sum + i) // 45
 The only thing I dislike at this point is that the usage of our `forLoop` is still limited. We have
 the signature.
 
-    int -> int -> (int -> int -> int) -> int
+    i:int -> stop:int -> f:(int -> int -> int) -> int
 
-It makes sense that `i` and `stop` are `int`. But usually we expect that the *looping-body* can work
-with any type. Or in other words, that `sum` can be of any type. Currently we are limited to `int`
-because we call `loop i 0` and directly pass `0` (an `int`) as `sum`. It makes more sense that the
-user can provide the *initial-value*. And `f` just returns a new value of the same type. This
-way, the *initial-value* can be generic.
+It makes sense that `i` and `stop` are `int`. But usually we expect that the *looping-body* (`f`)
+can work with any type. Or in other words, that `sum` can be of any type. And that a `forLoop`
+also can return any type. But currently we are limited to `int`.
+
+It is an `int` because we call `loop i 0` and directly pass `0` (an `int`) as `sum`.
+It makes more sense that the user can provide the *initial-value*. And `f` just returns a new value
+of the same type. This way, the *initial-value* can be generic.
 *)
 
 let rec forLoop i stop init f =
@@ -264,7 +271,7 @@ let sum = forLoop 0 10 0 (fun i sum -> sum + i) // 45
 (**
 We now have the signature:
 
-    int -> int -> 'a -> (int -> 'a -> 'a) -> 'a
+    i:int -> stop:int -> init:'a -> f:(int -> 'a -> 'a) -> 'a
 
 We now created a `forLoop` just with recursion and without any *mutable variable*. Our loop supports
 the creation of any value. The function `f` just returns the next state (`acc`) that is used for
@@ -277,6 +284,17 @@ build another list from it in an *immutable* way.
 let listOfNumbers = forLoop 0 10 [] (fun i list -> i :: list)
 // [9;8;7;6;5;4;3;2;1;0]
 
+(** Or build a string *)
+
+let stringofNumbers = forLoop 0 10 "" (fun i str -> str + (string i))
+// "0123456789"
+
+(** Or start with a value and just repeatedly call a function on it *)
+
+let x = 100.0
+let y = forLoop 0 5 x (fun i x -> x / 2.0)
+// 3.125
+
 (**
 ## Creating an immutable `foreach`
 
@@ -286,7 +304,7 @@ In C# we have `foreach` for this kind of idea. For example we can loop over a li
     [lang=csharp]
     var sum = 0;
     foreach ( var x in list ) {
-        sum = sum + x
+        sum = sum + x;
     }
 
 We now want to do the same but with an *immutable F# list* instead, so how do we loop an
@@ -304,35 +322,36 @@ tail // [2;3;4;5]
 
 (**
 Doing the extraction with a `let` is usually discouraged because such an operation could fail.
-If we have an empty list for example we couldn't extract the first element of a list. With
-Pattern matching we can specify multiple cases that we can check. But as you already can imagine
-again. This must be recursive again! Because we repeatedly want to extract one element from `tail`
-and we sure don't want to write:
+If we have an empty list for example we couldn't extract the first element of a list
+(and the code throws an exception). With Pattern matching we can specify multiple cases that we
+can check. But as you already can imagine again. This must be recursive again! Because we
+repeatedly want to extract one element from `tail` and we sure don't want to write:
 *)
 
 let (head1::tail1) = numbers2 // 1 [2;3;4;5]
 let (head2::tail2) = tail1    // 2 [3;4;5]
 let (head3::tail3) = tail2    // 3 [4;5]
+// and so on ...
 
 (**
 And on top, we need an exit condition. We want to end as soon we end up with an empty list for `tail`.
 Now let's do the same stuff we did with our `forLoop`. We expect some *initial value* that we can
-specific as an *accumulator*. The `f` function now just gets the list element and our *accumulator*.
+specify as an *accumulator*. The `f` function now just gets the list element and our *accumulator*.
 Once we end up with an empty list, we just return the *accumulator*. We call this function `fold`.
 *)
 
 let fold f (acc:'State) list =
     let rec loop remainingList acc =
         match remainingList with
-        | []         -> acc  // We return "acc" when we looped through the list
+        | []         -> acc  // We return "acc" when we reached the end of the list
         | head::tail ->      // extract first element of remainingList
             let nextAcc = f acc head // Compute the next accumulator
-            loop tail nextAcc        // Recurse again with the new remaining list
+            loop tail nextAcc        // Recurse with remaining list "tail" and "nextAcc"
     loop list acc
 
 (**
-I also changed the order of the arguments compared to the `forEach` function. We now
-can `sum` a list of `int`s like that:
+I also changed the order of the arguments compared to the `forEach` function. Now `f` comes first
+followed by `acc` and `list`. Summing a list of `int`s can now be written as:
 *)
 
 let sum = fold (fun acc x -> acc + x) 0 [1 .. 9] // 45
@@ -340,9 +359,15 @@ let sum = fold (fun acc x -> acc + x) 0 [1 .. 9] // 45
 (**
 So, what is `fold` exactly? `fold` is just the idea of looping through a *data-structure*
 in an *immutable way*. Instead of accessing a *mutable variable* that you access and *mutate* while
-you are looping, you provide a function as the *loop-body*. The purpose of this function is to
-calculate the next *accumulator* for the next iteration. Once the looping finished, `fold`
-returns the *accumulator* you build.
+you are looping, you provide a function as the *loop-body* and an *initial-accumulator*. 
+
+In looping you then have access to the current element and a *mutable variable* that you can
+modify. In `fold` your `f` function you get the current element and the last state as
+function arguments. Instead of *mutating* a variable outside of a loop. In `fold` you just
+return the new state that will be used for the next recursive call.
+
+Your *accumulator* will be returned as a result once you traversed all elements of
+your data-structure.
 
 ## Creating `map`
 
@@ -367,9 +392,9 @@ and not *append* elements. As we cannot *append* we have to provide another solu
 Besides a `fold` we usually also provide a `foldBack`. A `foldBack` just traverses a data-structure
 backwards or from *right-to-left* instead of *left-to-right*.
 
-But we also cannot easily go through a list backwards, an easy fix to this solution is when we implement
-`foldBack` by first *reversing* the input list and then use `fold`. After we have `foldBack` we can 
-define `map` with `foldBack` instead of `fold`.
+But we also cannot easily go through a list backwards, an easy fix to this problem is when we
+implement `foldBack` by first *reversing* the input list and then use `fold` on it. After we
+have `foldBack` we can define `map` with `foldBack` instead of `fold`.
 *)
 
 // Reverses a List
